49947bdb48af6a1c8db8a7d091e4fbffe1771b40
dotnetCarpenter <jon.ronnenberg@gmail.com>
2020-12-29 22:14:55 +0100

    fix command arguments to livereload and enable job control for the fg command

--- design/gradient_donut/server.sh
+++ design/gradient_donut/server.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+# enable job control for fg: https://stackoverflow.com/a/11824420/205696
+set -m
+
 # install node dependencies if node_modules does not exist
 if [ ! -d "./node_modules" ]; then
     npm ci
 fi
 
-npx livereload & python3 -m http.server 9000 && fg
+npx livereload . --debug & python3 -m http.server 9000 && fg
